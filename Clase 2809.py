96b19200aff9226ec8b4e76a764dba9e4a06fca3
Nico Gomez <nicog851@gmail.com>
2024-10-19 11:00:23 -0300

    hola

---
+++ Clase 2809.py
@@ -0,0 +1,14 @@
+#Datos personales y como presentarlos con concatenacion
+
+#VARIBLES
+
+nombre= "Leandro Nicolas"
+apellido= "Gomez"
+edad= 30
+
+#PROBLEMA 
+
+presentar= "Mi nombre es "+nombre+" "+apellido+", y tengo "+str(edad)+" años" 
+
+#RESOLUCION. 
+print (presentar)
---
+++ practicacoder.py
@@ -0,0 +1,10 @@
+#Primer ejercicio
+
+numero_1= input ("ingrese el primer valor: ")
+numero_2= input ("ingrese el segundo valor: ")
+resultado= int(numero_1) + int(numero_2)
+
+print (resultado)
+
+#Segundo Ejercicio
+
